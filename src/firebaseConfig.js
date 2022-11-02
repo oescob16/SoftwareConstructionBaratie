8bfdaa4a7103c346f4f188e60d152476dd3a6387
Justus frausto <jfrausto10@gmail.com>
2022-11-02 17:59:29 -0600

    Configuration files || DO NOT TOUCH

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBZiOp9t_Orzgm8PQlZB5QNzF9mt_CXkY4",
+    authDomain: "softwareconstructionbaratie.firebaseapp.com",
+    projectId: "softwareconstructionbaratie",
+    storageBucket: "softwareconstructionbaratie.appspot.com",
+    messagingSenderId: "298658687745",
+    appId: "1:298658687745:web:b087cae08882b11f50c900",
+    measurementId: "G-748VR9HX3T"
+  };

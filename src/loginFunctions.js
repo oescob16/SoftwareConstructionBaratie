a811351efa54c87a9fdf51c647083b51a49e8b49
Justus frausto <jfrausto10@gmail.com>
2022-11-09 14:35:16 -0700

    added logic for modal in login

--- src/loginFunctions.js
+++ src/loginFunctions.js
@@ -1,18 +1,25 @@
-//  The loginFunctions class has methods used by mainly by the popups in index.html. Additionaly, they allow the website to be dynamic
-// ----------------------------------------Team 6--------------------------------------------------
-import { AuthErrorCodes } from 'firebase/auth';
 
-// Show the login popup when the home button is clicked
-var el1 = document.getElementById('homeBtn');
-if(el1){
-  el1.addEventListener("click",function(){
-    document.querySelector(".popup").style.display="flex";
-})
+var modal = document.getElementById("myModal");
+    
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
 }
-// Hide the login popup when the close button is clicked 
-var el2 = document.getElementById('closeBtn');
-if(el2){
-  el2.addEventListener("click",function(){
-    document.querySelector(".popup").style.display="none";
-})
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }

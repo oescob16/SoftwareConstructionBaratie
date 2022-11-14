ee7ba5d4efd44f3290b4d58dc9637af46b30325d
Eric Smith <ericsmith@Erics-MacBook-Pro-2.local>
2022-11-14 01:43:59 -0700

    Got update fields working

--- src/user.js
+++ src/user.js
@@ -10,6 +10,7 @@ import {
     onSnapshot,
     getDocs,
     DocumentSnapshot,
+    updateDoc,
     setDoc,
 } from 'firebase/firestore'
 
@@ -66,13 +67,8 @@ function updateUserData(user){
             const newEmail = document.getElementById("editEmailLive").value;
 
             console.log("The email you entered: ", newEmail);
-
-            updateEmail(auth.currentUser, newEmail).then(() => {
-                db.collection("Users").doc(user.uid).update({email: newEmail});
-                console.log("Your name has been updated!");
-            }).catch((error) => {
-                console.log("Could not update your name!")
-            });
+            
+            editEmail(user, newEmail);
         })
     }
 
@@ -92,14 +88,7 @@ function updateUserData(user){
 
             console.log("Tried to update name to ", newName);
 
-            updateProfile(auth.currentUser, {
-                Name: newName
-            }).then(() => {
-                db.collection("Users").doc(user.uid).update({Name: newName});
-                console.log("Your name has been updated!");
-            }).catch((error) => {
-                console.log("Could not update your name!")
-            });
+            editUsername(user, newName);
         })
     }
 
@@ -132,12 +121,22 @@ function editUsername(pAuth, newName){
     updateProfile(auth.currentUser, {
         Name: newName
     }).then(() => {
-        db.collection("Users").doc(pAuth.uid).update({Name: newName});
         console.log("Your name has been updated!");
+        editUsernameInFirebase(pAuth, newName);
     }).catch((error) => {
         console.log("Could not update your name!")
+        console.log(error);
     });
     return;
+}
+
+async function editUsernameInFirebase(pAuth, newName){
+    userData = getUserData(pAuth);
+    await setDoc(doc(db, 'Users', pAuth.uid), {
+        Name: newName,
+        password: userData['password'],
+        email: userData['email']
+    });
 }
 
 function editPassword(pAuth, newPassword){
@@ -145,10 +144,21 @@ function editPassword(pAuth, newPassword){
         Password: newPassword
     }).then(() => {
         console.log("Your password has been updated!");
+        editPasswordInFirebase(pAuth, newName);
     }).catch((error) => {
-        console.log("Could not update your password!")
+        console.log("Could not update your password!");
+        console.log(error);
     });
     return;
+}
+
+async function editPasswordInFirebase(pAuth, newPassword){
+    userData = getUserData(pAuth);
+    await setDoc(doc(db, 'Users', pAuth.uid), {
+        Name: userData['name'],
+        password: newPassword,
+        email: userData['email']
+    });
 }
 
 function editEmail(pAuth, newEmail){
@@ -157,9 +167,19 @@ function editEmail(pAuth, newEmail){
     }).then(() => {
         console.log("Your Email has been updated!");
     }).catch((error) => {
-        console.log("Could not update your password!")
+        console.log("Could not update your password!");
+        console.log(error);
     });
     return;
+}
+
+async function editEmailInFirebase(pAuth, newEmail){
+    userData = getUserData(pAuth);
+    await setDoc(doc(db, 'Users', pAuth.uid), {
+        Name: userData['name'],
+        password: userData['password'],
+        email: newEmail
+    });
 }
 
 function displayName(usersName){
@@ -178,6 +198,7 @@ function displayUserData(user){
                 let name = ChildSnapshot.id;
                 let userName = ChildSnapshot.get('Name');
                 let userEmail = ChildSnapshot.get('email');
+                let userPassword = ChildSnapshot.get('password');
                 if(user.email == userEmail){
                     displayName(userName);
                     displayEmail(userEmail)
@@ -187,6 +208,29 @@ function displayUserData(user){
     }).catch(err => {
         console.log(err.message)
     })
+}
+
+function getUserData(user){
+    userDetails = [];
+    getDocs(usersRef).then(snapshot => {//
+        console.log(snapshot.docs)
+        snapshot.docs.forEach(
+            function(ChildSnapshot){
+                let name = ChildSnapshot.id;
+                let userName = ChildSnapshot.get('Name');
+                let userEmail = ChildSnapshot.get('email');
+                let userPassword = ChildSnapshot.get('password');
+                if(user.email == userEmail){
+                    userDetails['password'] = userPassword;
+                    userDetails['email'] = userEmail;
+                    userDetails['name'] = userName;
+                }
+            }
+        );
+    }).catch(err => {
+        console.log(err.message)
+    })
+    return userDetails;
 }
 
 

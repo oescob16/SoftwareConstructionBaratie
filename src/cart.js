5679417c776f6f77c6a2c30d2d9a91bb7feeee70
ilopez2511 <Gapgaps9000@gmail.com>
2022-11-15 20:04:33 -0700

    Added comments and cleaned code on cart.js

--- src/cart.js
+++ src/cart.js
@@ -40,7 +40,6 @@ initializeApp(firebaseConfig);
 
 
 // init services
-console.log("hello");
 const db = getFirestore();
 const auth = getAuth();
 
@@ -53,24 +52,11 @@ onAuthStateChanged(auth, (user) => {
 
       const userDocRef = doc(db,"Users/"+user.uid)
       const userCartcolref = collection(db,"Users/"+user.uid+"/CartItems");
-
-      // var itemTable = document.getElementById("item_list")
-      // var button = document.getElementById("plus1")
-      // let cartItems = []
-      // function alterTotal() {
-      //   let total_expense = getElementById("total");
-      //   cartArray((array) => {
-      //     let sum = 0;
-      //     array.forEach(element => {
-      //       sum = sum + element[1];
-      //     });
-      //     total_expense.innerHTML = "Total: " + sum;
-      //   })
-      // }
-
+      //This function allows us to extract/callback an array of food items to any part of our code.
       function cartArray(callBack) {
         let cartItems = []
         onSnapshot(userCartcolref, (snapshot) => {
+          //Pushes all the documents in our snapshot into an array cartItems
           snapshot.docs.forEach((doc) => {
             cartItems.push([doc.id, doc.data().price, doc.data().quantity]);
           })
@@ -81,31 +67,22 @@ onAuthStateChanged(auth, (user) => {
       }
 
         onSnapshot(userCartcolref, (snapshot) => {
+          //Uses snapshots of the docs to create a table row entry of the food, price and quantity
           snapshot.docs.forEach((doc,index) => { 
             addItemtoTable(doc.id, doc.data().price, doc.data().quantity,index)
           })
 
           console.log("onSnapShot")
         })
-
-      // function load(){
-      //   onSnapshot(userCartcolref, (snapshot) => {
-      //     snapshot.docs.forEach((doc,index) => { 
-      //       addItemtoTable(doc.id, doc.data().price, doc.data().quantity,index)
-      //     })
-
-      //     console.log("onSnapShot")
-      //   })
-      // }
-
-
+      
+      //Constants used to create the buttons +, - and Remove
       const plus_val = "+";
       const minus_val = "-";
       const del_val = 'Remove';
       const total = document.getElementById("total");
       
       changeTotal();
-
+      //This function serves to display the total amount to pay for the ordered food.
       function changeTotal() {
         cartArray((array) => {
           let count = 0;
@@ -115,7 +92,7 @@ onAuthStateChanged(auth, (user) => {
           total.innerHTML = "<b> Total: </b> $"+count;
         })
       }
-
+      //This function is used to increment the quantity of the item ordered by the user.
       function incrementQuantity(event) {
         const clicked = event.target;
         cartArray((array) =>{
@@ -124,21 +101,17 @@ onAuthStateChanged(auth, (user) => {
           var tbody = document.getElementById("item_list");
           tbody.innerHTML = '';
           total.innerHTML = '';
+          //Updates the quantity of the item by adding 1 to it when the button is pressed.
           updateDoc(cart_item, {
             quantity: array[clicked.id][2] + 1
           })
+          //Reloads the page.
           .then(()=>{
             location.reload();
           })
-          // getDocs(userCartcolref)
-          //   .then(snapshot => {
-          //      snapshot.docs.forEach((doc,index) => {
-          //       addItemtoTable(doc.id, doc.data().price, doc.data().quantity,index)
-          //      })
-          //    })
         });
       }
-      
+      //Function used to decrement the quantity of an item ordered by the user.
       function decrementQuantity(event) {
         const clicked = event.target;
         cartArray((array) =>{
@@ -147,27 +120,20 @@ onAuthStateChanged(auth, (user) => {
           var tbody = document.getElementById("item_list");
           tbody.innerHTML = '';
           total.innerHTML = '';
+          //Updates the items quantity by subtracting 1 from it.
           updateDoc(cart_item, {
             quantity: array[clicked.id-1][2] - 1
           })
           .then(()=>{
             location.reload();
           })
-          // getDocs(userCartcolref)
-          //   .then(snapshot => {
-          //      snapshot.docs.forEach((doc,index) => {
-          //       addItemtoTable(doc.id, doc.data().price, doc.data().quantity,index)
-          //      })
-          //    })
         });
       }
-
+      //Deletes an item from the users cart list.
       function deleteItem(event) {
         const clicked = event.target;
         const id_d = parseInt(clicked.id.charAt(1));
         cartArray((array) =>{
-          // alert(array[id_d][0]);
-          //console.log(array[clicked.id-1])
           const cart_item = doc(db,"Users/"+user.uid+"/CartItems/"+array[id_d][0]);
           var tbody = document.getElementById("item_list");
           tbody.innerHTML = '';
@@ -176,18 +142,12 @@ onAuthStateChanged(auth, (user) => {
           .then(()=>{
             location.reload();
           })
-          // getDocs(userCartcolref)
-          //   .then(snapshot => {
-          //      snapshot.docs.forEach((doc,index) => {
-          //       addItemtoTable(doc.id, doc.data().price, doc.data().quantity,index)
-          //      })
-          //    })
         });
       }
-
+      //Function used to add a new entry into our cart table adding items based on the users cart item collection.
       function addItemtoTable(name,price,quantity,index){
         var tbody1 = document.getElementById("item_list");
-        
+        //Creates the rows and buttons for each entry in the table adding index and class names to the buttons.
         var trow = document.createElement("tr");
         var td0 = document.createElement("td");
         var td1 = document.createElement("td");
@@ -231,162 +191,3 @@ onAuthStateChanged(auth, (user) => {
   }
 })
 
-
-
-
-// const colRef = collection(db, 'Users');
-// console.log(auth.currentUser);
-// const currUser = auth.currentUser;
-// console.log(currUser);
-// const userDocRef = doc(db,"Users/"+"e1WRimGxuINs1hKIUqLVeaJZEw43")
-// const userCartcolref = collection(db,"Users/"+"e1WRimGxuINs1hKIUqLVeaJZEw43"+"/CartItems");
-
-// // var itemTable = document.getElementById("item_list")
-// // var button = document.getElementById("plus1")
-// // let cartItems = []
-
-
-
-// function cartArray(callBack) {
-//   let cartItems = []
-//   onSnapshot(userCartcolref, (snapshot) => {
-//     snapshot.docs.forEach((doc) => {
-//       cartItems.push([doc.id, doc.data().price, doc.data().quantity]);
-//     })
-//     callBack(cartItems)
-//   })
-
-//   console.log("cartArray")
-// }
-
-//   onSnapshot(userCartcolref, (snapshot) => {
-//     snapshot.docs.forEach((doc,index) => { 
-//       addItemtoTable(doc.id, doc.data().price, doc.data().quantity,index)
-//     })
-
-//     console.log("onSnapShot")
-//   })
-
-// // function load(){
-// //   onSnapshot(userCartcolref, (snapshot) => {
-// //     snapshot.docs.forEach((doc,index) => { 
-// //       addItemtoTable(doc.id, doc.data().price, doc.data().quantity,index)
-// //     })
-
-// //     console.log("onSnapShot")
-// //   })
-// // }
-
-
-// const plus_val = "+";
-// const minus_val = "-";
-// //var plus = document.createElement("button");
-// //var minus = document.createElement("button");
-
-// function incrementQuantity(event) {
-//   const clicked = event.target;
-//   console.log("0000000000000000000")
-//   cartArray((array) =>{
-//     console.log(array[clicked.id])
-//     console.log("11111111111111111")
-//     const cart_item = doc(db,"Users/"+"e1WRimGxuINs1hKIUqLVeaJZEw43"+"/CartItems/"+array[clicked.id][0]);
-//     console.log("222222222222222222")
-//     var tbody = document.getElementById("item_list");
-//     tbody.innerHTML = '';
-//     console.log("3333333333333333333")
-//     updateDoc(cart_item, {
-//       quantity: array[clicked.id][2] + 1
-//     })
-//     .then(()=>{
-//       location.reload();
-//     })
-//     // getDocs(userCartcolref)
-//     //   .then(snapshot => {
-//     //      snapshot.docs.forEach((doc,index) => {
-//     //       addItemtoTable(doc.id, doc.data().price, doc.data().quantity,index)
-//     //      })
-//     //    })
-//   });
-// }
-
-// function addItemtoTable(name,price,quantity,index){
-//   var tbody1 = document.getElementById("item_list");
-  
-//   var trow = document.createElement("tr");
-//   var td0 = document.createElement("td");
-//   var td1 = document.createElement("td");
-//   var td2 = document.createElement("td");
-//   var plus = document.createElement("button");
-//   plus.id = index;
-
-//   //minus.id = index;
-  
-//   td0.innerHTML = name;
-//   td1.innerHTML = price;
-//   td2.innerHTML = quantity;
-//   plus.innerHTML = plus_val;
-//   plus.onclick = incrementQuantity;
-//   // minus.innerHTML = minus_val;
-//   trow.appendChild(td0);
-//   trow.appendChild(td1);
-//   trow.appendChild(td2);
-//   trow.appendChild(plus);
-//   tbody1.appendChild(trow);
-//   console.log("addItemtoTable")
-// }
-
-
-//console.log(cartItems)
-
-// function getCartArray(callBack) {
-//   let cartItems = []
-//   getDocs(userCartcolref)
-//   .then(snapshot => {
-//     snapshot.docs.forEach(doc => {
-//         cartItems.push([doc.data().price, doc.id])
-//     })
-//     callBack(cartItems)
-//   })
-//   .catch(err => {
-//     console.log(err.message)
-//   });
-// }
-
-// cartArray((array) =>{
-//   console.log(array)
-// });
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// const nameOfFoolItem = "ham2";
-// const priceofitem = 98;
-// //   console.log(cartItems[1][3])
-// const userCart = doc(db,"Users/"+"e1WRimGxuINs1hKIUqLVeaJZEw43"+"/CartItems/" + nameOfFoolItem);
-
-
-// setDoc(userCart,{
-//   price: priceofitem,
-//   quantity: priceofitem,
-//   spanishName: nameOfFoolItem,
-// } )
-
-// const newprice = "123456789"
-// updateDoc(userCart, { 
-//   price: newprice,
-//   });
-
-
-// const tranHistory = collection(db,"Users/"+"e1WRimGxuINs1hKIUqLVeaJZEw43"+"/transHistory");
-
-// getDocs(tranHistory)
-//   .then(snapshot => {
-//     console.log(snapshot.docs)
-//     let PrevItems = []
-//     snapshot.docs.forEach(doc => {
-//         PrevItems.push({ ...doc.data(), id: doc.id, price: doc.price})
-//     })
-//     console.log(PrevItems)
-//   })
-//   .catch(err => {
-//     console.log(err.message)
-//   })

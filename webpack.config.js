e15b21ccb47355118caecfef41a7e2a97ee33d78
Justus frausto <jfrausto10@gmail.com>
2022-12-04 00:50:46 -0700

    Fixed code smells

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,3 @@
-// const path = require('path')
-
-// module.exports = {
-//   mode: 'development',
-//   entry: {
-//     index: './src/index.js',
-//     signUp: './src/signUp.js',
-//     cart: './src/cart.js',
-//   },
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: '[name].bundle.js'
-//   },
-//   watch: true
-// }
 const path = require('path')
 
 module.exports = {
